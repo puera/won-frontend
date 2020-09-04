5468f679990118bf30e72ccb1849f5a34c683ebb
Renann Souza <renann_puera@hotmail.com>
2020-09-04 19:50:47 -0300

    💄 chore: Add more props styles on Heading Component

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,19 +1,33 @@
+import { HTMLAttributes } from 'react'
 import * as S from './styles'
 
+export type LineColorsProps = 'primary' | 'secondary'
+
 export type HeadingProps = {
-  children: React.ReactNode
   color?: 'white' | 'black'
   lineLeft?: boolean
   lineBottom?: boolean
-}
+  lineColor?: LineColorsProps
+  size?: 'small' | 'medium'
+} & HTMLAttributes<HTMLHeadElement>
 
 const Heading = ({
   children,
   color = 'white',
   lineLeft = false,
-  lineBottom = false
+  lineBottom = false,
+  size = 'medium',
+  lineColor = 'primary',
+  ...props
 }: HeadingProps) => (
-  <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+  <S.Wrapper
+    color={color}
+    lineLeft={lineLeft}
+    lineBottom={lineBottom}
+    lineColor={lineColor}
+    size={size}
+    {...props}
+  >
     {children}
   </S.Wrapper>
 )
