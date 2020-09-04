5b4abcd5e5a31d146474e85dc1f7f808eb114354
Renann Souza <renann_puera@hotmail.com>
2020-09-04 16:10:24 -0300

    :bento: chore: Create a Button Component with storybook and tests

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,24 @@
+import { ButtonHTMLAttributes, ReactNode } from 'react'
+import * as S from './styles'
+
+export type ButtonProps = {
+  children?: ReactNode
+  size?: 'small' | 'medium' | 'large'
+  fullWidth?: boolean
+  icon?: ReactNode
+} & ButtonHTMLAttributes<HTMLButtonElement>
+
+const Button = ({
+  children,
+  icon,
+  size = 'medium',
+  fullWidth = false,
+  ...props
+}: ButtonProps) => (
+  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon} {...props}>
+    {!!icon && icon}
+    {!!children && <span>{children}</span>}
+  </S.Wrapper>
+)
+
+export default Button
