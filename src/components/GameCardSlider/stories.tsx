93f54ec171db61787c87885e1c50972ed5f8d5c5
Renann Souza <renann_puera@hotmail.com>
2020-09-25 12:39:34 -0300

    💄 chore: Add GameCardSlider Component with storybook and tests

---
+++ src/components/GameCardSlider/stories.tsx
@@ -0,0 +1,67 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+import { GameCardProps } from 'components/GameCard'
+import GameCardSlider from '.'
+
+const items = [
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/red-dead-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/red-dead-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/red-dead-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/red-dead-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/red-dead-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/red-dead-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  }
+]
+
+export default {
+  title: 'GameCardSlider',
+  component: GameCardSlider,
+  args: { items },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<GameCardProps[]> = (args) => (
+  <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+    <GameCardSlider items={args} {...args} />
+  </div>
+)
---
+++ src/components/GameCardSlider/test.tsx
@@ -0,0 +1,61 @@
+import 'match-media-mock'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import GameCardSlider from '.'
+
+const items = [
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/red-dead-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/red-dead-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/red-dead-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/red-dead-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: '/img/red-dead-img.jpg',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
+  }
+]
+
+describe('<GameSlider />', () => {
+  it('should render with 4 active items', () => {
+    const { container } = renderWithTheme(<GameCardSlider items={items} />)
+    expect(container.querySelectorAll('.slick-active')).toHaveLength(4)
+  })
+
+  it('should render white arrows if color passed', () => {
+    renderWithTheme(<GameCardSlider items={items} color="white" />)
+
+    expect(screen.getByLabelText(/previous games/i)).toHaveStyle({
+      color: '#FAFAFA'
+    })
+    expect(screen.getByLabelText(/next games/i)).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+})
