bc85ce8e20ab19c7efbea1f3ada2555ec502ae02
Renann Souza <renann_puera@hotmail.com>
2020-10-17 16:55:22 -0300

    💄 chore: Add Radio Component with storybook and tests

---
+++ src/components/Radio/index.tsx
@@ -0,0 +1,45 @@
+import { InputHTMLAttributes, useCallback } from 'react'
+
+import * as S from './styles'
+
+type RadioValue = string | ReadonlyArray<string> | number
+
+export type RadioProps = {
+  onCheck?: (value?: RadioValue) => void
+  label?: string
+  labelColor?: 'white' | 'black'
+  labelFor?: string
+  value?: RadioValue
+} & InputHTMLAttributes<HTMLInputElement>
+
+const Radio = ({
+  label,
+  onCheck,
+  labelColor = 'white',
+  labelFor = '',
+  value,
+  ...props
+}: RadioProps) => {
+  const onChange = useCallback(() => {
+    if (onCheck) onCheck(value)
+  }, [onCheck, value])
+
+  return (
+    <S.Wrapper>
+      <S.Input
+        id={labelFor}
+        type="radio"
+        value={value}
+        onChange={onChange}
+        {...props}
+      />
+      {!!label && (
+        <S.Label labelColor={labelColor} htmlFor={labelFor}>
+          {label}
+        </S.Label>
+      )}
+    </S.Wrapper>
+  )
+}
+
+export default Radio
