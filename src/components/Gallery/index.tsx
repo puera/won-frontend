aeb2228d6b97560ccce20f98c505cd7e80b71b8c
Renann Souza <renann_puera@hotmail.com>
2020-11-17 20:13:17 -0300

    💄 chore: Add Gallery component with storybook and tests

---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,113 @@
+import { useEffect, useRef, useState } from 'react'
+import Slider, { SliderSettings } from 'components/Slider'
+import {
+  ArrowBackIos as ArrowLeft,
+  ArrowForwardIos as ArrowRight,
+  Close
+} from '@styled-icons/material-outlined'
+import * as S from './styles'
+import SlickSlider from 'react-slick'
+
+const commonSettings: SliderSettings = {
+  infinite: false,
+  lazyLoad: 'ondemand',
+  arrows: true,
+  nextArrow: <ArrowRight aria-label="next images" />,
+  prevArrow: <ArrowLeft aria-label="previous images" />
+}
+
+const settings: SliderSettings = {
+  ...commonSettings,
+  slidesToShow: 4,
+  responsive: [
+    {
+      breakpoint: 1375,
+      settings: {
+        arrows: false,
+        slidesToShow: 3.2
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: false,
+        slidesToShow: 2.2
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        slidesToShow: 2.2,
+        draggable: true
+      }
+    }
+  ]
+}
+
+const modalSettings: SliderSettings = {
+  ...commonSettings,
+  slidesToShow: 1
+}
+
+export type GalleryImageProps = {
+  src: string
+  label: string
+}
+
+export type GalleryProps = {
+  items: GalleryImageProps[]
+}
+
+const Gallery = ({ items }: GalleryProps) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const slider = useRef<SlickSlider>(null)
+
+  useEffect(() => {
+    const handleKeyUp = ({ key }: KeyboardEvent) => {
+      key === 'Escape' && setIsOpen(false)
+    }
+
+    window.addEventListener('keyup', handleKeyUp)
+    return () => window.removeEventListener('keyup', handleKeyUp)
+  }, [])
+
+  return (
+    <S.Wrapper>
+      <Slider ref={slider} settings={settings}>
+        {items.map((item, index) => (
+          <img
+            onClick={() => {
+              setIsOpen(true)
+              slider.current?.slickGoTo(index, true)
+            }}
+            role="button"
+            key={`thumb-${index}`}
+            src={item.src}
+            alt={`Thumb - ${item.label}`}
+          />
+        ))}
+      </Slider>
+
+      <S.Modal isOpen={isOpen} aria-label="modal" aria-hidden={!isOpen}>
+        <S.Close
+          role="button"
+          aria-label="close modal"
+          onClick={() => setIsOpen(false)}
+        >
+          <Close size={40} />
+        </S.Close>
+
+        <S.Content>
+          <Slider ref={slider} settings={modalSettings}>
+            {items.map((item, index) => (
+              <img key={`gallery-${index}`} src={item.src} alt={item.label} />
+            ))}
+          </Slider>
+        </S.Content>
+      </S.Modal>
+    </S.Wrapper>
+  )
+}
+
+export default Gallery
---
+++ src/components/Gallery/mock.ts
@@ -0,0 +1,26 @@
+export default [
+  {
+    src: '/img/red-dead-img.jpg',
+    label: 'Gallery Image 1'
+  },
+  {
+    src: '/img/red-dead-img.jpg',
+    label: 'Gallery Image 2'
+  },
+  {
+    src: '/img/red-dead-img.jpg',
+    label: 'Gallery Image 3'
+  },
+  {
+    src: '/img/red-dead-img.jpg',
+    label: 'Gallery Image 4'
+  },
+  {
+    src: '/img/red-dead-img.jpg',
+    label: 'Gallery Image 5'
+  },
+  {
+    src: '/img/red-dead-img.jpg',
+    label: 'Gallery Image 6'
+  }
+]
